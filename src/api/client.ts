146d964fc0925bbd2a72930894cdb9167017020b
ndjassapfranck7-gif <ndjassapfranck7@gmail.com>
2026-08-17 16:05:46 +0100

    COM5

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 ﻿import ky from "ky";
 
 export const apiClient = ky.create({
-  prefix: "https://dummyjson.com",
+  prefixUrl: "https://dummyjson.com",
   timeout: 10000,
   retry: {
     limit: 1,
